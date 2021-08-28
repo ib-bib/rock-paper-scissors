5fcf0202da66c07a50ecee671161206870289f2e
ib-bib <81387641+ib-bib@users.noreply.github.com>
2021-08-28 16:35:33 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+var userScr = 0;
+var compScr= 0;
+const userScore_span = document.getElementById("UserScore");
+const compScore_span = document.getElementById("CompScore");
+const scoreboard_dive = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("R");
+const paper_div = document.getElementById("P");
+const scissors_div = document.getElementById("S");
+
+function getCompChoice(){
+    const choices = ["R", "S", "P"];
+    const randNum = Math.floor( Math.random() * 3);
+    return choices[randNum];
+}
+
+function convertLetter(x) {
+    if (x === "R") return "Rock";
+    if (x === "P") return "Paper";
+    return "Scissors";
+}
+
+function win(user, comp) {
+    userScr++;
+    userScore_span.innerHTML = userScr;
+    result_p.innerHTML = `${convertLetter(user)} beats ${convertLetter(comp)}!\nYou win!`;
+}
+
+function lose(user, comp) {
+    compScr++;
+    compScore_span.innerHTML = compScr;
+    result_p.innerHTML =` ${convertLetter(comp)} beats ${convertLetter(user)}!\nYou lose...`;
+}
+
+function draw() {
+    userScr++;
+    compScr++;
+    userScore_span.innerHTML = userScr;
+    compScore_span.innerHTML = compScr;
+    result_p.innerHTML = "A DRAW!";
+}
+
+function gameFunc(userPick){
+    const compPick = getCompChoice();
+    switch (userPick + compPick) {
+        case "RS":
+        case "SP":
+        case "PR":
+            win(userPick, compPick);
+            break;
+        case "SR":
+        case "PS":
+        case "RP":
+            lose(userPick, compPick);
+            break;
+        case "PP":
+        case "RR":
+        case "SS":
+            draw();
+            break;
+    }
+}
+
+rock_div.addEventListener("click", function() {
+    gameFunc("R");
+})
+
+paper_div.addEventListener("click", function() {
+    gameFunc("P");
+})
+
+scissors_div.addEventListener("click", function() {
+    gameFunc("S");
+})

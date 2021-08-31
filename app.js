df65ace0d2134101e3b231aefa0b297fb1e560a5
ib-bib <81387641+ib-bib@users.noreply.github.com>
2021-08-31 14:40:43 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var userScr = 0;
 var compScr= 0;
 const userScore_span = document.getElementById("UserScore");
 const compScore_span = document.getElementById("CompScore");
-const scoreboard_dive = document.querySelector(".score-board");
+const scoreboard_div = document.querySelector(".score-board");
+const userShoot_span = document.getElementById("YouPick");
+const compShoot_span = document.getElementById("CpuPick");
 const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("R");
 const paper_div = document.getElementById("P");
 const scissors_div = document.getElementById("S");
-const uWord = "You".fontsize(3).sub();
-const cWord = "AI".fontsize(3).sub();
+const uWord = "(You)".fontsize(3).sub();
+const cWord = "(AI)".fontsize(3).sub();
 
 function getCompChoice(){
     const choices = ["R", "S", "P"];
@@ -22,28 +24,90 @@ function convertLetter(x) {
     return "Scissors";
 }
 
+function getUserImage(chosen){
+    var image = document.createElement("img");
+    if(chosen === "R") {
+        image.src = 'rock.png';
+        image.alt = "Rock";
+    }
+    if(chosen === "P") {
+        image.src = 'paper.png';
+        image.alt = "Paper";
+    }
+    if( chosen === "S") {
+        image.src = 'scissors.png';
+        image.alt = "Scissors";
+    }
+    image.width = "100";
+    image.height = "100";
+    placeUserImage(image);
+}
+
+function getCompImage(chosen) {
+    var image = document.createElement("img");
+    if(chosen === "R") {
+        image.src = 'rock.png';
+        image.alt = "Rock";
+    }
+    if(chosen === "P") {
+        image.src = 'paper.png';
+        image.alt = "Paper";
+    }
+    if( chosen === "S") {
+        image.src = 'scissors.png';
+        image.alt = "Scissors";
+    }
+    image.width = "100";
+    image.height = "100";
+    placeCompImage(image);
+}
+
+function placeUserImage(pic) {
+    if ( userShoot_span.hasChildNodes() ) {
+        var oldPic = userShoot_span.lastChild;
+        userShoot_span.replaceChild(pic, oldPic);
+    }
+    userShoot_span.appendChild(pic);
+}
+
+function placeCompImage(pic) {
+    if ( compShoot_span.hasChildNodes() ) {
+        var oldPic = compShoot_span.lastChild;
+        compShoot_span.replaceChild(pic, oldPic);
+    }
+    compShoot_span.appendChild(pic);
+}
+
 function win(user, comp) {
+    var where = document.getElementById(user);
     userScr++;
     userScore_span.innerHTML = userScr;
-    result_p.innerHTML = `${convertLetter(user)}${uWord} beats ${convertLetter(comp)}${cWord}!\nYou win!`;
+    result_p.innerHTML = `${convertLetter(user)}${uWord} beats ${convertLetter(comp)}${cWord}!\nYou win! :D`;
+    where.classList.add('greenGlow');
+    setTimeout(() => where.classList.remove('greenGlow'), 450 );
 }
 
 function lose(user, comp) {
+    var where = document.getElementById(user);
     compScr++;
     compScore_span.innerHTML = compScr;
-    result_p.innerHTML =` ${convertLetter(comp)}${cWord} beats ${convertLetter(user)}${uWord}!\nYou lose...`;
+    result_p.innerHTML =` ${convertLetter(comp)}${cWord} beats ${convertLetter(user)}${uWord}!\nYou lose... :C`;
+    where.classList.add('redGlow');
+    setTimeout( () => where.classList.remove('redGlow'), 450 );
 }
 
-function draw() {
-    userScr++;
-    compScr++;
+function draw(user) {
+    var where = document.getElementById(user);
     userScore_span.innerHTML = userScr;
     compScore_span.innerHTML = compScr;
     result_p.innerHTML = "A DRAW!";
+    where.classList.add('blueGlow');
+    setTimeout(() => where.classList.remove('blueGlow'), 450 );
 }
 
 function gameFunc(userPick){
     const compPick = getCompChoice();
+    getCompImage(compPick);
     switch (userPick + compPick) {
         case "RS":
         case "SP":
@@ -58,19 +122,22 @@ function gameFunc(userPick){
         case "PP":
         case "RR":
         case "SS":
-            draw();
+            draw(userPick);
             break;
     }
 }
 
 rock_div.addEventListener("click", function() {
     gameFunc("R");
+    getUserImage("R");
 })
 
 paper_div.addEventListener("click", function() {
     gameFunc("P");
+    getUserImage("P");
 })
 
 scissors_div.addEventListener("click", function() {
     gameFunc("S");
+    getUserImage("S");
 })
